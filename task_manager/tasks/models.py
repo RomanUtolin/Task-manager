5af259e7ec494d8935a7a3ae87f12a593c6b7a75
RomanUtolin <romautolin@gmail.com>
2023-05-24 16:59:50 +0300

    add Whitenoise and setup Flake8

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -8,11 +8,17 @@ from django.utils.translation import gettext_lazy as _
 class Task(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'))
     description = models.TextField(blank=True, verbose_name=_('Description'))
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name=_('Status'), related_name='status')
-    executor = models.ForeignKey(User, blank=True, on_delete=models.PROTECT, verbose_name=_('Executor'),
+    status = models.ForeignKey(Status, on_delete=models.PROTECT,
+                               verbose_name=_('Status'),
+                               related_name='status')
+    executor = models.ForeignKey(User, blank=True, on_delete=models.PROTECT,
+                                 verbose_name=_('Executor'),
                                  related_name='executor')
-    autor = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_('Autor'), related_name='autor')
-    labels = models.ManyToManyField(Label, blank=True, through="TaskLabels", verbose_name=_('Labels'))
+    autor = models.ForeignKey(User, on_delete=models.PROTECT,
+                              verbose_name=_('Autor'),
+                              related_name='autor')
+    labels = models.ManyToManyField(Label, blank=True, through="TaskLabels",
+                                    verbose_name=_('Labels'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
